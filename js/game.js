cbea7b1c68a1bed5be4e0e1c34c2ef2bfa076dc8
mitchelclow <mitchelclow@gmail.com>
2017-05-17 23:42:16 -0600

    wrote pseudocode for game.js

---
+++ js/game.js
@@ -0,0 +1,40 @@
+		var letters = ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"]
+
+
+		var winCounter = 0;
+		var lossCounter = 0;
+		var guessesRemainingCounter = 9;
+		var guessesSoFarCounter = [i];
+
+		for (var i = 0; i < letters.length; i++) {
+
+			$(computerGuess)
+			$(userGuess)
+
+			$(document).ready(function() {
+
+		      // VARIABLES
+		      // ====================================================================
+		      // Here we create variables for tracking the number of sandwiches eaten
+		      // ...
+		      $("#letters").on("click", function() {
+
+		      	if{
+		      		(#userGuess) = (#computerGuess)
+
+		      		winCounter++
+		      	
+		      	else
+
+		       $("#letters").on("click", function() {
+		      	lossCounter++
+
+		      });
+
+		      $("#letters").on("click", function() {
+		      	guessesRemainingCounter--
+
+		      });
+
+		  }
+		});
