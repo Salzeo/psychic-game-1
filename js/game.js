b108ea4303a701a5b1ef43ab76bf48674bd3ea50
mitchelclow <mitchelclow@gmail.com>
2017-05-19 14:39:40 -0600

    got the javascript basics working

--- js/game.js
+++ js/game.js
@@ -1,40 +1,57 @@
-		var letters = ["a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z"]
+		
+
+		// for (var i = 0; i < letters.length; i++) {
+
+		// 	$(computerGuess)
+		// 	$(userGuess)
+
+		// 	$(document).ready(function() {
+
+		//       // VARIABLES
+		//       // ====================================================================
+		//       // Here we create variables for tracking the number of sandwiches eaten
+		//       // ...
+		//       $("#letters").on("click", function() {
+
+		//       	if{
+		//       		(#userGuess) = (#computerGuess)
+
+		//       		winCounter++
+		      	
+		//       	else
+
+		//        $("#letters").on("click", function() {
+		//       	lossCounter++
+
+		//       });
+
+		//       $("#letters").on("click", function() {
+		//       	guessesRemainingCounter--
+
+		//       });
+
+		//   }
+	
+
+
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
 		var winCounter = 0;
 		var lossCounter = 0;
 		var guessesRemainingCounter = 9;
-		var guessesSoFarCounter = [i];
+		var guessesSoFarCounter = 0;
 
-		for (var i = 0; i < letters.length; i++) {
+	document.onkeyup = function(){
+		var userGuess = String.fromCharCode(event.keyCode).
+			toLowerCase();
 
-			$(computerGuess)
-			$(userGuess)
+		console.log(userGuess);
 
-			$(document).ready(function() {
+		var computerGuess = letters[Math.floor(Math.random()*letters.length)];
 
-		      // VARIABLES
-		      // ====================================================================
-		      // Here we create variables for tracking the number of sandwiches eaten
-		      // ...
-		      $("#letters").on("click", function() {
+		console.log(computerGuess);
 
-		      	if{
-		      		(#userGuess) = (#computerGuess)
 
-		      		winCounter++
-		      	
-		      	else
-
-		       $("#letters").on("click", function() {
-		      	lossCounter++
-
-		      });
-
-		      $("#letters").on("click", function() {
-		      	guessesRemainingCounter--
-
-		      });
-
-		  }
-		});
+	};
