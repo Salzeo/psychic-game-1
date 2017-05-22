f308d53d6575efc050b08b56416ab2e7496b610b
mitchelclow <mitchelclow@gmail.com>
2017-05-21 23:44:59 -0600

    final push, got the guesses resetting

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 
 		var winCounter = 1;
-		var lossCounter = 0;
+		var lossCounter = 1;
 		var guessesRemainingCounter = 8;
 		var guessesSoFarCounter = "";
 		var computerEntry
@@ -17,7 +17,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 
 	document.onkeyup = function(keyPressed) {
 	userEntry = keyPressed.key;
-	$("#soFar").html("Guesses so far: " + userEntry)
+	$("#soFar").append(userEntry)
 
 
 		if (userEntry === computerEntry) {
@@ -37,6 +37,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 			guessesRemainingCounter = 9
 			$("#userGuess").html("Guesses left: " + guessesRemainingCounter);
 			$("#userLosses").html("Losses: " + lossCounter++);
+			$("#soFar").html("");
 			console.log(lossCounter);
 			computerEntry = letters[Math.floor(Math.random()*letters.length)];
 			console.log(computerEntry);
