35ed5ec32637b6f092f92ce436db278866ea1b6c
mitchelclow <mitchelclow@gmail.com>
2017-05-21 22:55:27 -0600

    pushing to get help to get guesses remaining counter to reset

--- js/game.js
+++ js/game.js
@@ -32,95 +32,17 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 			console.log(guessesRemainingCounter);
 		}
 
+		if (guessesRemainingCounter < 1) {
+			$("#guessesRemainingCounter")[0].reset();
+
+		}
+
 			}
 			$("#userLosses").html("Losses: " + lossCounter--);
 			console.log(lossCounter);
-
-
-		// console.log(guessesRemainingCounter)
 			
 
 			
 			
 			
 });
-										
-
-											
-
-
-
-
-		
-		// var userGuess = String.fromCharCode(event.keyCode).
-		// 	toLowerCase();
-
-		// console.log(computerGuess);
-
-		// $("#userWins").html("Wins: " + winCounter)
-		// console.log(winCounter)
-
-		// $("#userLosses").html("Losses: " + lossCounter)
-		// console.log(lossCounter)
-
-		// $("#userGuess").html("Guesses left: " + guessesRemainingCounter)
-		// console.log(guessesRemainingCounter)
-
-
-		// document.getElementById((userGuess==computerGuess))  [wins++, document.getElementById("winCounter").innerHTML = ("winCounter: " + winCounter++)]
-
-
-
-// $(document).ready(function() {
-
-// 	var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	
-
-// 	var computerGuess = letters[Math.floor(Math.random()*letters.length)];
-// 	var userGuess = "";
-
-// 	console.log(computerGuess);
-
-// 	var winCounter = 0;
-// 	var lossCounter = 0;
-// 	var guessesRemainingCounter = 9;
-// 	var guessesSoFarCounter = 0;
-
-// 	for (var i = 0; i < letters.length; i++) {
-// 		if (userGuess === computerGuess){
-// 			winCounter++
-// 		}
-// 		else {
-// 		}
-
-
-			
-// 	}
-
-
-// 	document.onkeyup = function(){
-// 		var userGuess = String.fromCharCode(event.keyCode);
-// 		toLowerCase();
-
-// 		console.log(userGuess);
-
-// 		// getElementById("#winCounter");
-// 		lossCounter++;
-// 		guessesRemainingCounter--;
-
-
-// 		$("#userWins").html("Wins: " + winCounter);
-// 		console.log(winCounter);
-
-// 		$("#userLosses").html("Losses: " + lossCounter);
-// 		console.log(lossCounter);
-
-// 		$("#userGuess").html("Guesses left: " + guessesRemainingCounter);
-// 		console.log(guessesRemainingCounter);
-
-// 		$("#soFar").append(userGuess);
-// 		console.log(userGuess);
-// 	};
-
-
-// });
